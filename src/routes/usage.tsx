984a20288d67f57aecd80e017c98a404cc691d0f
abidmmp <abidmmp99@gmail.com>
2021-05-25 10:17:00 +0500

    Optimize useLocalStorage performance

--- src/routes/usage.tsx
+++ src/routes/usage.tsx
@@ -36,3 +36,21 @@ export function useDebounce<T extends (...args: any[]) => void>(fn: T, delay: nu
     timer.current = setTimeout(() => fn(...args), delay);
   }, [fn, delay]) as T;
 }
+
+
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T>(key: string, initial: T) {
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initial;
+    } catch {
+      return initial;
+    }
+  });
+  useEffect(() => {
+    try { localStorage.setItem(key, JSON.stringify(value)); } catch {}
+  }, [key, value]);
+  return [value, setValue] as const;
+}
