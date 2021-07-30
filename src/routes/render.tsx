d2fa820cc697dc97739d413cb507d84b5bdc2564
abidmmp <abidmmp99@gmail.com>
2021-07-30 10:17:00 +0500

    Optimize useLocalStorage performance

--- src/routes/render.tsx
+++ src/routes/render.tsx
@@ -206,3 +206,21 @@ function RenderCompleteBanner({ project, onDismiss }: { project: string; onDismi
     </div>
   );
 }
+
+
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T>(key: string, initial: T) {
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initial;
+    } catch {
+      return initial;
+    }
+  });
+  useEffect(() => {
+    try { localStorage.setItem(key, JSON.stringify(value)); } catch {}
+  }, [key, value]);
+  return [value, setValue] as const;
+}
