7724b16b86b96ea82d2c1cc4e598ded3b64ed1d1
abidmmp <abidmmp99@gmail.com>
2022-03-15 09:00:00 +0500

    Update Input helper method

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -19,3 +19,27 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<"tex
 Textarea.displayName = "Textarea";
 
 export { Textarea };
+
+
+import { forwardRef, InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error?: string;
+  hint?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, hint, id, ...props }, ref) => {
+    const inputId = id ?? label.toLowerCase().replace(/\s+/g, '-');
+    return (
+      <div className="form-group">
+        <label htmlFor={inputId} className="form-label">{label}</label>
+        <input ref={ref} id={inputId} className={`form-input ${error ? 'error' : ''}`} {...props} />
+        {hint && !error && <p className="form-hint">{hint}</p>}
+        {error && <p className="form-error" role="alert">{error}</p>}
+      </div>
+    );
+  }
+);
+Input.displayName = 'Input';
