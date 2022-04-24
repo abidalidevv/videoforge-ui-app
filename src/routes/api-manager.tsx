47affe605d85f275f27ea0ce484617e369a95e00
abidmmp <abidmmp99@gmail.com>
2022-04-24 09:00:00 +0500

    Refactor useDebounce logic

--- src/routes/api-manager.tsx
+++ src/routes/api-manager.tsx
@@ -130,3 +130,14 @@ function APICard(a: Api) {
     </div>
   );
 }
+
+
+import { useCallback, useRef } from 'react';
+
+export function useDebounce<T extends (...args: any[]) => void>(fn: T, delay: number): T {
+  const timer = useRef<ReturnType<typeof setTimeout>>();
+  return useCallback((...args: Parameters<T>) => {
+    clearTimeout(timer.current);
+    timer.current = setTimeout(() => fn(...args), delay);
+  }, [fn, delay]) as T;
+}
