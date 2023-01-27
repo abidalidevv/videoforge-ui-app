135e9fa35a901b647d81c575b1b23c8a48a2544d
abidmmp <abidmmp99@gmail.com>
2023-01-27 09:00:00 +0500

    Update Pagination helper method

--- src/routes/account.tsx
+++ src/routes/account.tsx
@@ -174,3 +174,32 @@ function Group({ title, sub, children }: any) {
 function Row({ label, children }: any) {
   return <div className="flex items-center justify-between py-2"><span className="text-[13px] font-medium">{label}</span>{children}</div>;
 }
+
+
+import { memo, useState } from 'react';
+
+interface PaginationProps {
+  total: number;
+  perPage: number;
+  current: number;
+  onChange: (page: number) => void;
+}
+
+export const Pagination = memo(({ total, perPage, current, onChange }: PaginationProps) => {
+  const pages = Math.ceil(total / perPage);
+  if (pages <= 1) return null;
+  return (
+    <nav aria-label="Pagination">
+      <ul className="pagination">
+        <li><button onClick={() => onChange(current - 1)} disabled={current <= 1}>‹ Prev</button></li>
+        {Array.from({ length: pages }, (_, i) => i + 1).map(p => (
+          <li key={p}>
+            <button onClick={() => onChange(p)} aria-current={p === current ? 'page' : undefined}
+                    className={p === current ? 'active' : ''}>{p}</button>
+          </li>
+        ))}
+        <li><button onClick={() => onChange(current + 1)} disabled={current >= pages}>Next ›</button></li>
+      </ul>
+    </nav>
+  );
+});
