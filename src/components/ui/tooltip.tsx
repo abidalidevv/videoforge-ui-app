10ba43cfe767b79e595a69b10fd5bbf9eed72f79
abidmmp <abidmmp99@gmail.com>
2023-07-20 11:34:00 +0500

    Implement useClickOutside functionality

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -30,3 +30,20 @@ const TooltipContent = React.forwardRef<
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
 export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
+
+
+import { useEffect, useRef, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(
+  handler: () => void
+): RefObject<T> {
+  const ref = useRef<T>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) handler();
+    };
+    document.addEventListener('mousedown', listener);
+    return () => document.removeEventListener('mousedown', listener);
+  }, [handler]);
+  return ref;
+}
