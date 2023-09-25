708b405b183a4b9f4316ac686ee6d6f7633df7f2
abidmmp <abidmmp99@gmail.com>
2023-09-25 09:00:00 +0500

    Implement useToggle functionality

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -37,3 +37,21 @@ export function useFetch<T>(url: string): FetchState<T> {
   }, [url]);
   return state;
 }
+
+
+import { useState, useCallback } from 'react';
+
+interface UseToggleReturn {
+  value: boolean;
+  toggle: () => void;
+  setTrue: () => void;
+  setFalse: () => void;
+}
+
+export function useToggle(initial = false): UseToggleReturn {
+  const [value, setValue] = useState(initial);
+  const toggle   = useCallback(() => setValue(v => !v), []);
+  const setTrue  = useCallback(() => setValue(true), []);
+  const setFalse = useCallback(() => setValue(false), []);
+  return { value, toggle, setTrue, setFalse };
+}
