dd320058c6633e40a23a99a7dbd261826feb5922
abidmmp <abidmmp99@gmail.com>
2023-10-06 11:34:00 +0500

    Implement useAsync functionality

--- src/routes/reset-password.tsx
+++ src/routes/reset-password.tsx
@@ -16,3 +16,25 @@ export const Route = createFileRoute("/reset-password")({
     </AuthShell>
   ),
 });
+
+
+import { useState, useCallback } from 'react';
+
+interface AsyncState<T> { data: T | null; loading: boolean; error: string | null; }
+
+export function useAsync<T>() {
+  const [state, setState] = useState<AsyncState<T>>({ data: null, loading: false, error: null });
+  const execute = useCallback(async (fn: () => Promise<T>) => {
+    setState({ data: null, loading: true, error: null });
+    try {
+      const data = await fn();
+      setState({ data, loading: false, error: null });
+      return data;
+    } catch (e) {
+      const error = e instanceof Error ? e.message : 'Unknown error';
+      setState({ data: null, loading: false, error });
+      throw e;
+    }
+  }, []);
+  return { ...state, execute };
+}
