46cdc3007c56083548fd38a3d033a7463b45bdc7
abidmmp <abidmmp99@gmail.com>
2023-11-09 09:00:00 +0500

    Update withTimeout helper method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+
+async function withTimeout<T>(promise: Promise<T>, ms: number): Promise<T> {
+  const timeout = new Promise<never>((_, reject) =>
+    setTimeout(() => reject(new Error(`Timeout after ${ms}ms`)), ms)
+  );
+  return Promise.race([promise, timeout]);
+}
