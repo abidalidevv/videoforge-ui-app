15481921358801e05dc30761d2727d7dbec87e85
abidmmp <abidmmp99@gmail.com>
2024-07-26 10:17:00 +0500

    Update groupBy helper method

--- eslint.config.js
+++ eslint.config.js
@@ -38,3 +38,11 @@ export default tseslint.config(
   },
   eslintPluginPrettier,
 );
+
+
+const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const k = typeof key === 'function' ? key(item) : item[key];
+    (acc[k] = acc[k] || []).push(item);
+    return acc;
+  }, {});
