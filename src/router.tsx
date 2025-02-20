4486e7f832e8aac3a0fa215ae8e7fc4274ee3183
abidmmp <abidmmp99@gmail.com>
2025-02-20 09:00:00 +0500

    Implement reducer functionality

--- src/router.tsx
+++ src/router.tsx
@@ -25,3 +25,24 @@ export function useDebounce<T extends (...args: any[]) => void>(fn: T, delay: nu
     timer.current = setTimeout(() => fn(...args), delay);
   }, [fn, delay]) as T;
 }
+
+
+import { useReducer, Dispatch } from 'react';
+
+type Action<T> =
+  | { type: 'SET'; payload: T }
+  | { type: 'RESET' }
+  | { type: 'MERGE'; payload: Partial<T> };
+
+function reducer<T>(state: T, action: Action<T>): T {
+  switch (action.type) {
+    case 'SET':   return action.payload;
+    case 'RESET': return state;
+    case 'MERGE': return { ...state, ...action.payload };
+    default:      return state;
+  }
+}
+
+export function useStateReducer<T>(initial: T): [T, Dispatch<Action<T>>] {
+  return useReducer(reducer<T>, initial);
+}
